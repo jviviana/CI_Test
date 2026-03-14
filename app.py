9b7ec01a07d9172c9779488d6aa369704f20ba73
jviviana <jessica.viscue@correounivalle.edu.co>
2026-03-14 11:10:58 -0500

    app nuevo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def calcular_prioridad(dias_abiertos):
     """Calcula la prioridad de un caso basado en los días abiertos."""
     if dias_abiertos < 0:
         return "Error: Dias negativos"
-    elif dias_abiertos >5:
+    elif dias_abiertos > 5:
         return "Alta"
     else:
-        return "Normal" 
+        return "Normal"
