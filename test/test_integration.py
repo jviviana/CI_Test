f4a8844c0e15358ddd8b417de4064f11bf767c26
jviviana <jessica.viscue@correounivalle.edu.co>
2026-03-14 11:33:25 -0500

    test integration

---
+++ test/test_integration.py
@@ -0,0 +1,24 @@
+import sqlite3 
+from app import calcular_prioridad
+def testdb_connection_and_ticked_creation():
+    """Prueba de conexión a la base de datos."""
+
+    conn = sqlite3.connect(':memory:')  # Conexión a una base de datos en memoria para pruebas
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE TABLE tickets (id INTEGER, ubicacion TEXT, prioridad TEXT)"
+    )
+    
+    # logica de negocio ypersistencia
+    ubicacion = "Laboratorio de redes"
+    prioridad= calcular_prioridad(10)  # Dedebería retornar "Alta" para 10 días abiertos
+    cursor.execute(
+        "INSERT INTO tickets (id, ubicacion, prioridad) VALUES (?, ?, ?)", 
+        (1, ubicacion, prioridad)
+    )
+    conn.commit()
+    # verificacion (assert)
+    cursor.execute("SELECT prioridad FROM tickets WHERE id = 1")
+    resultado = cursor.fetchone()
+    assert resultado[0] == "Alta"
+    conn.close()
