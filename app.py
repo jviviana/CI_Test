13f9ed7803a656bd722a9599519198b204866656
jviviana <jessica.viscue@correounivalle.edu.co>
2026-03-14 10:34:23 -0500

    test app Unitario

---
+++ app.py
@@ -0,0 +1,8 @@
+def calcular_prioridad(dias_abiertos):
+    """Calcula la prioridad de un caso basado en los días abiertos."""
+    if dias_abiertos < 0:
+        return "Error: Dias negativos"
+    elif dias_abiertos >5:
+        return "Alta"
+    else:
+        return "Normal" 
